8ca8b4d3b6c88a910371d6a96241eaf6b3faeb11
WayneLu <wayne23123@gmail.com>
2023-08-03 10:16:53 +0800

    首頁第一次提交

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,44 @@
 <script setup></script>
 
 <template>
-  <div>首頁</div>
+  <div class="sectionHolder"></div>
+  <div class="disCen title">
+    <div class="dis spa titleCard">
+      <div>
+        <h1>Wayne's Blog</h1>
+        <h1>前端學習筆記</h1>
+        <h3>記錄一些關於前端學習的筆記</h3>
+      </div>
+      <img src="../assets/logo.svg" alt="" />
+    </div>
+  </div>
+  <div class="sectionHolder"></div>
+  <div>
+    <div></div>
+    <div></div>
+  </div>
+  <div class="sectionHolder"></div>
+  <div>4</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.sectionHolder {
+  width: 100vw;
+  max-width: 100%;
+  height: 100px;
+  background-color: #000;
+}
+
+.title {
+  background-color: #000;
+}
+
+.titleCard {
+  width: 500px;
+}
+
+img {
+  width: 100px;
+  height: 100px;
+}
+</style>
--- src/pages/Layout.vue
+++ src/pages/Layout.vue
@@ -1,10 +1,12 @@
 <script setup>
 import NavBar from "../components/NavBar.vue";
+import Footer from "../components/Footer.vue";
 </script>
 
 <template>
   <NavBar />
   <RouterView />
+  <Footer />
 </template>
 
 <style scoped></style>
